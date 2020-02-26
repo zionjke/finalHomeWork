9d5d018634dbac5fe010033f1ef1436ca360d8ca
zionjke <vincere802@gmail.com>
2020-02-27 00:32:12 +0200

    create Input and Button components and css modules

---
+++ src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import styles from '../css/Button.module.css'
+
+class Button extends React.Component {
+
+  render = () => {
+    return (
+        <button>
+
+        </button>
+    );
+  }
+}
+
+export default Button;
---
+++ src/components/Input.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../css/Input.module.css'
+
+class Input extends React.Component {
+
+  render = () => {
+    return (
+        <input type="text"/>
+    );
+  }
+}
+
+export default Input;
