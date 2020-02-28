1edb9888a8acbe2c0529767d70a43d9e799b8558
artem_vasiliev <vincere802@gmail.com>
2020-02-28 11:14:36 +0200

    Updates

---
+++ precache-manifest.ecad75a306bc5212c1ef1c18150ff13e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ecd6bcb9af18186519d13d568d0e851",
+    "url": "/finalHomework/index.html"
+  },
+  {
+    "revision": "786448a36bc0cc5f7323",
+    "url": "/finalHomework/static/css/main.810f31ce.chunk.css"
+  },
+  {
+    "revision": "2e9e6ebd217369b2f6bf",
+    "url": "/finalHomework/static/js/2.7d8adaad.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/finalHomework/static/js/2.7d8adaad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "786448a36bc0cc5f7323",
+    "url": "/finalHomework/static/js/main.2ac3ddff.chunk.js"
+  },
+  {
+    "revision": "f4d239ae47fbd77e2689",
+    "url": "/finalHomework/static/js/runtime-main.4f23c34a.js"
+  }
+]);
