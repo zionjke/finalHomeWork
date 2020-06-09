17ed32239eb987313a1eff3bb44c3585d7153ea2
zionjke <vincere802@gmail.com>
2020-06-09 21:17:37 +0300

    верстка

---
+++ src/components/TodoList/components/List/List.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import  './list.scss'
+
+
+const List = ({items}) => {
+    return (
+        <ul className="list">
+            {items.map(item => (
+                <li className={item.active ? 'active' : ""}>
+                    <i>
+                        {item.icon
+                            ? (item.icon)
+                            : (<i className={`badge badge--${item.color}`}/>
+                            )}
+                    </i>
+                    <span>{item.name}</span>
+                </li>
+            ))}
+
+        </ul>
+    )
+}
+
+export default List
