57cae0f38b9c778127ce6997dad93873227de311
zionjke <vincere802@gmail.com>
2020-06-20 15:23:13 +0300

    Updates

--- precache-manifest.70c8169f81a9a6a2b90dc06e8e3643c8.js
+++ precache-manifest.70c8169f81a9a6a2b90dc06e8e3643c8.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8ee41a901804d148badd7b5ace943f19",
+    "revision": "ba3d8cdd2eb754caddb2e005cbc634a2",
     "url": "/finalHomework/index.html"
   },
   {
-    "revision": "0c95b08a4871c9a21236",
+    "revision": "70b14380bf9c4a066113",
     "url": "/finalHomework/static/css/2.1097d493.chunk.css"
   },
   {
-    "revision": "aa021d1725a12956ec88",
+    "revision": "21cd5a02113ddf6e5d07",
     "url": "/finalHomework/static/css/main.b1352eb2.chunk.css"
   },
   {
-    "revision": "0c95b08a4871c9a21236",
-    "url": "/finalHomework/static/js/2.f87893ed.chunk.js"
+    "revision": "70b14380bf9c4a066113",
+    "url": "/finalHomework/static/js/2.153ec04a.chunk.js"
   },
   {
     "revision": "928d7b5eb39d16fe9a880722c974b51e",
-    "url": "/finalHomework/static/js/2.f87893ed.chunk.js.LICENSE.txt"
+    "url": "/finalHomework/static/js/2.153ec04a.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "aa021d1725a12956ec88",
-    "url": "/finalHomework/static/js/main.f7e30896.chunk.js"
+    "revision": "21cd5a02113ddf6e5d07",
+    "url": "/finalHomework/static/js/main.6907e824.chunk.js"
   },
   {
     "revision": "f4d239ae47fbd77e2689",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/finalHomework/precache-manifest.ceee5faac6cb37a88bcfbbe430bbbfea.js"
+  "/finalHomework/precache-manifest.70c8169f81a9a6a2b90dc06e8e3643c8.js"
 );
 
 self.addEventListener('message', (event) => {
